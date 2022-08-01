1b577b2d2a87b5265429084a370a20ba2a6e1c94
Brandon Fosso <brandon.fosso@gmail.com>
2022-08-01 14:48:23 -0500

    Compress DNA strings

---
+++ trivial_compression.py
@@ -0,0 +1,59 @@
+"""
+Classic Computer Science Problems in Python
+Chapter 1: Small Problems
+Section 2: Trivial Compression
+"""
+
+
+class CompressedGene:
+    def __init__(self, gene: str) -> None:
+        self._compress(gene)
+
+    def _compress(self, gene: str) -> None:
+        self.bit_string: int = 1
+        for nucleotide in gene.upper():
+            self.bit_string <<= 2
+            if nucleotide == "A":
+                self.bit_string |= 0b00
+            elif nucleotide == "C":
+                self.bit_string |= 0b01
+            elif nucleotide == "G":
+                self.bit_string |= 0b10
+            elif nucleotide == "T":
+                self.bit_string |= 0b11
+            else:
+                raise ValueError(f"Invalid Nucleotide: {nucleotide}")
+
+    def decompress(self) -> str:
+        gene: str = ""
+        for i in range(0, self.bit_string.bit_length() - 1, 2):
+            bits: int = self.bit_string >> i & 0b11
+            if bits == 0b00:
+                gene += "A"
+            elif bits == 0b01:
+                gene += "C"
+            elif bits == 0b10:
+                gene += "G"
+            elif bits == 0b11:
+                gene += "T"
+            else:
+                raise ValueError(f"Invalid bits: {bits}")
+        return gene[::-1]
+
+    def __str__(self) -> str:
+        return self.decompress()
+
+
+if __name__ == "__main__":
+    import random
+    import sys
+    import time
+    from sys import getsizeof
+
+    original: str = "".join(random.choices("ACTG", k=100)) * 100
+    print(f"Original is {getsizeof(original)} bytes")
+    compressed: CompressedGene = CompressedGene(original)
+    print(f"Compressed is {getsizeof(compressed.bit_string)} bytes")
+    print(compressed.bit_string)
+    same = original == compressed.decompress()
+    print(f"Original and decompressed are the same: {same}")
