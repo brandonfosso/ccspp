60165f4b92457441d000bf63e3c53b4e8e3a39b4
Brandon Fosso <brandon.fosso@gmail.com>
2022-08-02 11:32:10 -0500

    Lint only by defualt

--- noxfile.py
+++ noxfile.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 import nox
 
-nox.options.sessions = ["format", "lint"]
+nox.options.sessions = ("lint",)
 
 root_dir = Path(__file__).absolute().parent
 py_files = sorted(f.name for f in root_dir.iterdir() if f.suffix == ".py")
