795c413ecf3aaec65e73a9d189e42f6364bb6790
Brandon Fosso <brandon.fosso@gmail.com>
2022-08-02 15:56:24 -0500

    Clean up fib code

--- fib.py
+++ fib.py
@@ -11,19 +11,13 @@ from typing import Generator
 memo: Dict[int, int] = {0: 0, 1: 1}
 
 
-def fib1(n: int) -> int:
-    return fib1(n - 1) + fib1(n - 2)
-
-
 def fib2(n: int) -> int:
-    print("fib2 called")
     if n < 2:
         return n
     return fib2(n - 1) + fib2(n - 2)
 
 
 def fib3(n: int) -> int:
-    print("fib3 called")
     if n not in memo:
         memo[n] = fib3(n - 1) + fib3(n - 2)
     return memo[n]
@@ -31,14 +25,12 @@ def fib3(n: int) -> int:
 
 @lru_cache(maxsize=None)
 def fib4(n: int) -> int:
-    print("fib4 called")
     if n < 2:
         return n
     return fib4(n - 2) + fib4(n - 1)
 
 
 def fib5(n: int) -> int:
-    print("fib5 called")
     if n == 0:
         return n
     last: int = 0
